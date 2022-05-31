582347daaa425cbb3b84500fd0dac753a2750ae0
Kyle Frame <kjframe1223@gmail.com>
2022-05-30 19:35:03 -0500

    Update createSlice()

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -3,15 +3,43 @@ import { createSlice } from '@reduxjs/toolkit';
 import { baseUrl } from '../../app/shared/baseUrl';
 import { mapImageURL } from '../../utils/mapImageURL';
 
+
+export const fetchCampsites = createAsyncThunk(
+    'campsites/fetchCampsites',
+    async () => {
+        const response = await fetch(baseUrl + 'campsites');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
 const initialState = {
     campsitesArray: [],
     isLoading: true,
-    errMsg: '';
+    errMsg: ''
 };
 
 const campsitesSlice = createSlice({
     name: 'campsites',
-    initialState
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchCampsites.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchCampsites.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.campsitesArray = mapImageURL(action.payload);
+        },
+        [fetchCampsites.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message : 'Fetch failed';
+        }
+    }
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
@@ -29,15 +57,3 @@ export const selectCampsiteById = (id) => (state) => {
 export const selectFeaturedCampsite = (state) => {
     return state.campsites.campsitesArray.find((campsite) => campsite.featured);
 };
-
-export const fetchCampsites = createAsyncThunk(
-    'campsites/fetchCampsites',
-    async () => {
-        const response = await fetch(baseUrl + 'campsites');
-        if (!response.ok) {
-            return Promise.reject('Unable to fetch, status: ' + response.status);
-        }
-        const data = await response.json();
-        return data;
-    }
-);
